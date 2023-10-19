a5bccfa7bd4ff1672f788bb7d85e2b01b6b906da
Dylan To <dylan.to@hotmail.com>
2023-10-19 19:20:56 +0800

    Add api port to env

--- src/schemas/env.ts
+++ src/schemas/env.ts
@@ -5,6 +5,7 @@ export const envSchema = z.object({
   CLIENT_ID: z.string(),
   CLIENT_SECRET: z.string(),
   BOT_TOKEN: z.string(),
+  API_PORT: z.string().default("4000").transform(Number),
   NODE_ENV: z
     .enum(["development", "production", "test"])
     .default("development"),
