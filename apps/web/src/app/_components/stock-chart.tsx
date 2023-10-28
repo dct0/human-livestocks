2efe1e74635e223717c0fcb1e875ce106a8c685e
Dylan To <dylan.to@hotmail.com>
2023-10-29 00:35:25 +0800

    Reverse stocks data

--- apps/web/src/app/_components/stock-chart.tsx
+++ apps/web/src/app/_components/stock-chart.tsx
@@ -2,12 +2,16 @@
 
 import { LineChart } from "@tremor/react";
 import { type StockPrice } from "db";
+import { useMemo } from "react";
 
 export default function StockChart({ stocks }: { stocks: StockPrice[] }) {
-  const data = stocks.map((stockPrice) => ({
-    ...stockPrice,
-    createdAt: `${stockPrice.createdAt.toLocaleDateString()} ${stockPrice.createdAt.toLocaleTimeString()}`,
-  }));
-
+  const data = useMemo(() => {
+    return stocks
+      .map((stockPrice) => ({
+        ...stockPrice,
+        createdAt: `${stockPrice.createdAt.toLocaleDateString()} ${stockPrice.createdAt.toLocaleTimeString()}`,
+      }))
+      .toReversed();
+  }, [stocks]);
   return <LineChart data={data} index="createdAt" categories={["price"]} />;
 }
