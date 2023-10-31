922d2e9eb57a35119f4c7d092ed965acadc60cb1
Dylan To <dylan.to@hotmail.com>
2023-10-31 23:46:24 +0800

    Use nextauth_url as fallback for trpc

--- apps/web/src/trpc/shared.ts
+++ apps/web/src/trpc/shared.ts
@@ -8,11 +8,12 @@ export const transformer = superjson;
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
 
-  // Netlify -- dumb hack
-  if (process.env.FUCK_YOU) return process.env.NEXTAUTH_URL;
-
   // Vercel
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+
+  // Netlify -- dumb hack
+  if (process.env.FUCK_YOU ?? process.env.NEXTAUTH_URL)
+    return process.env.NEXTAUTH_URL;
 
   // Docker
   if (process.env.INTERNAL_ADDRESS)
