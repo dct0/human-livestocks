0226dc4cae6f4ff11e79693474e7598402de69b9
Dylan To <dylan.to@hotmail.com>
2024-01-30 21:34:49 +0800

    Fetch stocks clientside

--- apps/web/src/app/dashboard/[guildId]/my-stats/page.tsx
+++ apps/web/src/app/dashboard/[guildId]/my-stats/page.tsx
@@ -1,17 +1,14 @@
 import MessageCardContent from "@/app/_components/dashboard/message-card-content";
 import StockChart from "@/app/_components/stock-chart";
-import { api } from "@/trpc/server";
 import { Card, Col, Grid, Text, Title } from "@tremor/react";
 
-export default async function MyStats() {
-  const stocks = await api.stock.get.query({});
-
+export default function MyStats() {
   return (
     <Grid className="gap-4" numItemsMd={3} numItems={1}>
       <Col numColSpanMd={3} numColSpan={1}>
         <Card>
           <Title>Stock Price</Title>
-          <StockChart stocks={stocks} />
+          <StockChart />
         </Card>
       </Col>
       <Col numColSpanMd={2} numColSpan={1}>
