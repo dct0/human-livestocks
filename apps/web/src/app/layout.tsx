9a893cf817ff309fc04ce312a78a667e793c0c27
Dylan To <dylan.to@hotmail.com>
2024-02-02 21:55:12 +0800

    Copy some code

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
-import { headers } from "next/headers";
 
 import NavBar from "./_components/nav-bar";
 import Providers from "./providers";
@@ -21,8 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
-        {/* I hope this works how I think it does... */}
-        <Providers headers={headers()}>
+        <Providers>
           <NavBar />
           <main className="mx-auto flex min-h-screen flex-col bg-tremor-background-muted pt-20 dark:bg-dark-tremor-background-muted">
             {children}
--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,19 +1,12 @@
-"use client";
-
 import { TRPCReactProvider } from "@/trpc/react";
-import { type headers as nextHeaders } from "next/headers";
-import { Provider as JotaiProvider } from "jotai";
+import { cookies, type headers as nextHeaders } from "next/headers";
 
 export default function Providers({
-  headers,
   children,
 }: {
-  headers: ReturnType<typeof nextHeaders>;
   children: React.ReactNode;
 }) {
   return (
-    <JotaiProvider>
-      <TRPCReactProvider headers={headers}>{children}</TRPCReactProvider>
-    </JotaiProvider>
+      <TRPCReactProvider cookies={cookies().toString()}>{children}</TRPCReactProvider>
   );
 }
