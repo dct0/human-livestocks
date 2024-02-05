be1cb81d31fbaa831b7bfaf69cf6a9c8479cd91a
Dylan To <19729435+dct0@users.noreply.github.com>
2024-02-05 20:54:59 +0800

    Display top stocks (#39)
    * Display top stocks
    
    * Update prisma
    
    * Order stock prices by date

--- apps/web/src/app/dashboard/[guildId]/overview/page.tsx
+++ apps/web/src/app/dashboard/[guildId]/overview/page.tsx
@@ -1,6 +1,6 @@
 import MessageCardContent from "@/app/_components/dashboard/message-card-content";
-import StockChart from "@/app/_components/stock-chart";
 import { Card, Col, Grid, Title } from "@tremor/react";
+import TopStocksChart from "./top-stocks-chart";
 
 export default function Overview() {
   return (
@@ -8,8 +8,7 @@ export default function Overview() {
       <Col numColSpanMd={3} numColSpan={1}>
         <Card>
           <Title>Top stocks</Title>
-          {/* TODO: Show top x stocks */}
-          <StockChart />
+          <TopStocksChart />
         </Card>
       </Col>
       <Col numColSpanMd={1} numColSpan={1}>
