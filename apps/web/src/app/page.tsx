955f03130f66f659d906b2e58549ec92922b417f
fourilent <fourilent@frlt.one>
2024-03-13 19:23:47 +0800

    Change some classes

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -10,14 +10,14 @@ export default function Home() {
     <>
       <div className="flex max-w-5xl flex-1 flex-col-reverse items-center justify-center space-y-8 self-center md:flex-row md:space-x-8 md:px-8">
         <div className="space-y-3 text-tremor-content-emphasis md:space-y-8 dark:text-dark-tremor-content-emphasis">
-          <h1 className="text-3xl font-bold md:text-6xl">
+          <h1 className="text-3xl font-bold tracking-tight md:text-6xl">
             You are the commodity
           </h1>
-          <Subtitle className="text-xl text-tremor-content-emphasis md:text-3xl dark:text-dark-tremor-content-emphasis">
+          <Subtitle className="text-xl tracking-tight text-tremor-content-emphasis md:text-3xl dark:text-dark-tremor-content-emphasis">
             Your bests, your blunders. As a number.
           </Subtitle>
           <Link
-            className="!mt-4 inline-flex items-center justify-center rounded-tremor-default bg-icon-discord px-4 py-2 text-sm font-medium text-white shadow-tremor-input outline-none transition hover:bg-icon-discord-hover"
+            className="mt-4 inline-flex items-center justify-center rounded-tremor-default bg-icon-discord px-4 py-2 text-sm font-medium text-white shadow-tremor-input outline-none transition hover:bg-icon-discord-hover"
             href={env.NEXT_PUBLIC_INVITE_URL}
             target="_blank"
           >
